f3df1a715f5227397fb05c7af7b89ede3ad0cb75
Rukky <bunchofkeysdev@gmail.com>
2026-06-18 15:13:11 +0100

    the sum of a range complete

--- Chapter4/theSumOfARange.js
+++ Chapter4/theSumOfARange.js
@@ -20,8 +20,26 @@
 
 // Your code here.
 
+function range(start, end, step = 1) {
+  let arr = [];
+
+  for (let i = start; step < 0 ? i >= end : i <= end; i = i + step) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+function sum(arr) {
+  let sum = 0;
+  for (num of arr) {
+    sum += num;
+  }
+  return sum;
+}
 console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(1, 10, 2));
+// [ 1, 3, 5, 7, 9 ]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));

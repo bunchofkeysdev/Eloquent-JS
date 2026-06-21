7da2dc46cab0e1270bd85d28c15e44617574e042
Rukky <bunchofkeysdev@gmail.com>
2026-06-21 11:18:53 +0100

    completed aList exercise

---
+++ Chapter4/aList.js
@@ -0,0 +1,66 @@
+// As generic blobs of values, objects can be used to build all sorts of data structures.
+// A common data structure is the list (not to be confused with arrays). A list is a nested
+// set of objects, with the first object holding a reference to the second, the second to the
+//  third, and so on:
+
+// let list = {
+//   value: 1,
+//   rest: {
+//     value: 2,
+//     rest: {
+//       value: 3,
+//       rest: null
+//     }
+//   }
+// };
+// The resulting objects form a chain, as shown in the following diagram:
+
+// A diagram showing the memory structure of a linked list. There are 3 cells,
+// each with a value field holding a number, and a 'rest' field with an arrow
+// to the rest of the list. The first cell's arrow points at the second cell,
+// the second cell's arrow at the last cell, and the last cell's 'rest' field holds null.
+// A nice thing about lists is that they can share parts of their structure.
+// For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list}
+// (with list referring to the binding defined earlier), they are both independent lists,
+//  but they share the structure that makes up their last three elements.
+// The original list is also still a valid three-element list.
+
+// Write a function arrayToList that builds up a list structure like the one
+//  shown when given [1, 2, 3] as argument. Also write a listToArray function that
+// produces an array from a list. Add the helper functions prepend, which takes an element
+//  and a list and creates a new list that adds the element to the front of the input list,
+// and nth, which takes a list and a number and returns the element at the given position in
+// the list (with zero referring to the first element) or undefined when there is no such element.
+
+// If you haven’t already, also write a recursive version of nth.
+
+function arrayToList(arr) {
+  let list = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    list = { value: arr[i], rest: list };
+  }
+  return list;
+}
+function listToArray(list) {
+  let arr = [];
+  let current = list;
+  while (current !== null) {
+    arr.push(current.value);
+    current = current.rest;
+  }
+  return arr;
+}
+function prepend(value, list) {
+  return { value, rest: list };
+}
+function nth(list, num) {
+  return listToArray(list)[num];
+}
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
---
+++ Chapter4/reversingAnArray.js
@@ -0,0 +1,55 @@
+// Reversing an array
+// Arrays have a reverse method that changes the array by inverting the order
+// in which its elements appear. For this exercise, write two functions,
+// reverseArray and reverseArrayInPlace. The first, reverseArray,
+// should take an array as its argument and produce a new array
+// that has the same elements in the inverse order. The second,
+// reverseArrayInPlace, should do what the reverse method does:
+//  modify the array given as its argument by reversing its elements.
+// Neither may use the standard reverse method.
+
+// Thinking back to the notes about side effects and pure functions in
+// the previous chapter, which variant do you expect to be useful in more
+// situations? Which one runs faster?
+
+// Your code here.
+function reverseArray(arr) {
+  const newArr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    let temp = arr[i];
+    let oppositeIndex = arr.length - 1 - i;
+    arr[i] = arr[oppositeIndex];
+    arr[oppositeIndex] = temp;
+  }
+
+  return arr;
+}
+let myArray = ["A", "B", "C"];
+console.log(reverseArray(myArray));
+// → ["C", "B", "A"];
+console.log(myArray);
+// → ["A", "B", "C"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
+
+// function reverseArrayInPlace2(arr) {
+//   for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+//     let lastIndex = arr.length - 1 - i;
+//     let temp = arr[i];
+//     arr[i] = arr[lastIndex];
+//     arr[lastIndex] = temp;
+//   }
+//   return arr;
+// }
+// let arrayValue2 = [1, 2, 3, 4, 5];
+// reverseArrayInPlace2(arrayValue2);
+// console.log(arrayValue2);
